b783de3f2c87ae002c58201638fe03b1626acf21
Rodger <festool@festools-MacBook-Pro.local>
2015-01-25 21:56:01 -0600

    added git ignore

--- index.php
+++ index.php
@@ -1,10 +1,4 @@
 <?php
-try{
-    $dbh = new PDO('pgsql:host=localhost;port=5432;dbname=test;user=festool;password=butterscotch');
-}catch(PDOException $e){
-
-    echo $e->getMessage();
-}
 
 
 $sql = "select * from weather";
